88f84f7aad7fffba2343e8286b4e2c76516e6b76
Jose Manuel Lopera Franco <josemanuelloperafranco@joses-macbook-pro.local>
2016-08-14 15:43:07 -0500

    Initial Commit

---
+++ pruebaGit/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  pruebaGit
+//
+//  Created by Jose Manuel Lopera Franco on 14/08/16.
+//  Copyright © 2016 Jose Manuel Lopera Franco. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+
+}
+
